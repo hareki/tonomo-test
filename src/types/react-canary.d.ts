6c327614738e4632c15d9d537593b00ada40c38a
Tu Nguyen <minhtu1392000@gmail.com>
2026-06-26 14:56:55 +0700

    chore: remove react-canary type declaration

--- src/types/react-canary.d.ts
+++
@@ -1,3 +0,0 @@
-// App Router bundles React's canary channel, which is where `ViewTransition`
-// and `addTransitionType` live. Opt the project's types into that channel.
-/// <reference types="react/canary" />
